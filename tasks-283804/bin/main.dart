9f5167e0563125c9ae15b95ab89e5c0c665db1f0
Ilya_Azorin <ilyaazorin@list.ru>
2023-09-07 22:38:52 +0300

    feat(task): task-283804
    В репозиторий добавлен шаблон проекта для здания ООО "Травка - муравка"

---
+++ tasks-283804/bin/main.dart
@@ -0,0 +1,8 @@
+import 'package:surf_dart_courses_template/task_283804_lib.dart' as lib;
+
+/// Точка входа в программу.
+///
+/// [arguments] - список аргументов принимаемых программой.
+void main(List<String> arguments) {
+  print('Hello world: ${lib.calculate()}!');
+}
---
+++ tasks-283804/test/task_283804_test.dart
@@ -0,0 +1,8 @@
+import 'package:surf_dart_courses_template/task_283804_lib.dart' as lib;
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(lib.calculate(), 42);
+  });
+}
