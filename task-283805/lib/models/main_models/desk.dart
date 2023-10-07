cb29bb16ab1c894f2f02c4957367706cfdbb60df
Ilya_Azorin <ilyaazorin@list.ru>
2023-10-07 20:43:22 +0300

    feat: добавил именованный конструктор для создания конкретного базового стола
    Пунтк 2 из задания

--- task-283805/lib/models/main_models/desk.dart
+++ task-283805/lib/models/main_models/desk.dart
@@ -13,4 +13,24 @@ class Desk extends Table {
     required super.legsCount,
     required super.boxCount,
   });
+
+  /// Конструктор создания базового стола, который был на каждой кухне
+  Desk.simpleDesk()
+      : this(
+          'Простой стол',
+          'Описание очень простого стола',
+          Size(height: 750, width: 1800, depth: 800),
+          guid: 'simpleDesk_1',
+          boxCount: 0,
+          legsCount: 4,
+          weight: 10,
+          features: [],
+          materials: [
+            Material('ЛДСП', 'Самый распространенный материал для мебели', 8000,
+                guid: 'material_1',
+                manufacturer: Company(
+                    'ООО "Рога и копыта"', 'Настолько простая фирма, что даже подозрительно', 'Платформа 9 и 3/4',
+                    guid: 'company_1', registrationCountry: 'Великобритания'))
+          ],
+        );
 }
