09d1158ca64d2dee83214ad6492705517c413ca1
Ilya_Azorin <ilyaazorin@list.ru>
2023-10-16 23:33:17 +0300

    feat: выполнение условий задания по доработке кода

--- task_283807/bin/surf_dart_courses_template.dart
+++ task_283807/bin/surf_dart_courses_template.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
+/// Описание стратегии выйгрыша
+typedef Strategy = double Function(List<String> cardsOnDesk, List<String> cardsInHand);
+
 class PokerPlayer {
   /// Список текущих карт в руке у игрока
-  List<String> _currentHand = ['King of clubs', 'Nine of hearts'];
+  final List<String> _currentHand = ['King of clubs', 'Nine of hearts'];
 
   /// Субъективная оценка выигрыша
   double _surenessInWin = 0;
@@ -9,7 +14,7 @@ class PokerPlayer {
   void calculateProbabilities(
     List<String> cardOnDesk,
 
-    /// Это часть первого задания. [Strategy] пока не сущестивует.
+    /// Это часть первого задания. [Strategy] пока не существует.
     ///
     /// Опишите его.
     Strategy strategy,
@@ -20,11 +25,14 @@ class PokerPlayer {
 void main() {
   final opponent = PokerPlayer();
 
-  /// Это часть первого задания. [Strategy] пока не сущестивует.
+  /// Это часть первого задания. [Strategy] пока не существует.
   ///
   /// Опишите его.
   final Strategy fakeStrategy = (p0, p1) {
-    /// Ваш код - здесь
+    // вывод карт игрока в консоль в соответвии с заданием
+    p1.forEach((e) => print(e));
+    // возврат шанса выйгрыша для выполнения услоий описания стратегии
+    return Random().nextDouble() * 100;
   };
 
   opponent.calculateProbabilities(
