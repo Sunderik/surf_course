48e2a78ebd10b219957a489707f894efbc154ca8
Ilya_Azorin <ilyaazorin@list.ru>
2023-10-22 23:58:37 +0300

    feat:начал добавлять структуру классов

---
+++ task_283808/lib/enums/enums.dart
@@ -0,0 +1,5 @@
+export 'classes_enum.dart';
+export 'country_enum.dart';
+export 'genders_enum.dart';
+export 'strategies_enum.dart';
+export 'tools_enum.dart';
---
+++ task_283808/lib/models/models.dart
@@ -0,0 +1,2 @@
+export 'main_models/models.dart';
+export 'sub_models/models.dart';
