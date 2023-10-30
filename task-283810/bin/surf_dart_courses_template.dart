00e0fed7fec9c28a56bf8f91dcff80d37768e98f
Ilya_Azorin <ilyaazorin@list.ru>
2023-10-30 23:45:07 +0300

    feat: добавлен шаблон задания "Магазин "Шестёрочка" "

---
+++ task-283810/bin/surf_dart_courses_template.dart
@@ -0,0 +1,5 @@
+import 'package:task_283810/surf_dart_courses_template.dart' as surf_dart_courses_template;
+
+void main(List<String> arguments) {
+  print('Hello world: ${surf_dart_courses_template.calculate()}!');
+}
---
+++ task-283810/test/surf_dart_courses_template_test.dart
@@ -0,0 +1,8 @@
+import 'package:task_283810/surf_dart_courses_template.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
